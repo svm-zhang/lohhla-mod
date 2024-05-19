b7b9d48d4e551d3912da53ffc03f0869de1522b8
simo <svm.zhang@gmail.com>
2024-05-19 10:55:38 -0400

    initial commmit; build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/lib/R/library/hlalohReforged"
+
+# copy scripts to bin folder
+cp "$RECIPE_DIR/hlalohReforged.R" "$PREFIX/bin/hlalohReforged"
+cp "$RECIPE_DIR/plot.R" "$PREFIX/bin/hlalohplot"
+
+# make it executable
+chmod +x "$PREFIX"/bin/*
+
+# copy perl module to lib folder
+cp "$RECIPE_DIR/R/bamer.R" "$PREFIX/lib/R/library/hlalohReforged"
+cp "$RECIPE_DIR/R/cli.R" "$PREFIX/lib/R/library/hlalohReforged"
+cp "$RECIPE_DIR/R/loh.R" "$PREFIX/lib/R/library/hlalohReforged"
+cp "$RECIPE_DIR/R/pairwise_aln.R" "$PREFIX/lib/R/library/hlalohReforged"
+cp "$RECIPE_DIR/R/pathio.R" "$PREFIX/lib/R/library/hlalohReforged"
