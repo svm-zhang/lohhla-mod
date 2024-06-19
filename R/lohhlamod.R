fcc8ae53e8875afe4fa0a22617dd4b00d85c6a5f
simo <svm.zhang@gmail.com>
2024-06-19 10:58:22 -0700

    Rename

---
+++ R/lohhlamod.R
@@ -0,0 +1,154 @@
+#!/usr/bin/env -S Rscript --vanilla
+
+suppressPackageStartupMessages({
+  library(argparse)
+  library(data.table)
+  library(ggplot2)
+  library(splitstackshape)
+  library(seqinr)
+  library(Biostrings)
+  library(Rsamtools)
+})
+
+options(width = 600)
+
+
+extract_tstates <- function(tstate_est_file) {
+  print("[INFO] Getting estimates of ploidy and purity")
+  tstates_dt <- fread(tstate_est_file, drop = c(1))
+  if (!all(c("TumorPloidy", "TumorPurityNGS") %in% names(tstates_dt))) {
+    print(paste(
+      "[ERROR] Miss either purity, or ploidy, or both ",
+      "in the CNV model results",
+      sep = ""
+    ))
+    quit(status = 1)
+  }
+  list(
+    ploidy = tstates_dt[["TumorPloidy"]],
+    purity = tstates_dt[["TumorPurityNGS"]]
+  )
+}
+
+make_bins_old <- function(allele, aln, allele_length, bin_size = 150) {
+  start_pos <- aln$start
+  end_pos <- aln$end
+  bin_breaks <- seq(start_pos, end_pos, by = bin_size)
+  # the last bin can be less than 150, so merged with second last bin
+  # end_pos + 2 was from original code
+  bin_breaks <- c(bin_breaks[-length(bin_breaks)], end_pos + 2)
+  istarts <- bin_breaks[-length(bin_breaks)]
+  # this makes sure no end position of last bin not repeating as
+  # start position in the next bin
+  istarts[2:length(istarts)] <- istarts[2:length(istarts)] + 1
+  iends <- bin_breaks[2:length(bin_breaks)]
+  indices <- seq(1, length(istarts))
+
+  if (start_pos > 2) {
+    istarts <- c(1, istarts)
+    iends <- c(start_pos - 1, iends)
+    indices <- c(0, indices)
+  }
+  if (allele_length > max(iends)) {
+    istarts <- c(istarts, max(iends) + 1)
+    iends <- c(iends, allele_length)
+    indices <- c(indices, allele_length + 1)
+  }
+
+  bin_dt <- data.table(
+    seqnames = allele, start = istarts, end = iends, bin = indices
+  )
+  bin_dt[, end := ifelse(end > allele_length, allele_length, end)]
+  print(bin_dt)
+  stop()
+
+  bin_dt
+}
+
+initialize_libs <- function() {
+  pkg_name <- "hlalohReforged"
+  libpaths <- file.path(.libPaths(), pkg_name)
+  lib_pattern <- "(bamer|cli|loh|pairwise_aln|pathio).R"
+  rscripts <- list.files(libpaths, lib_pattern, full.names = TRUE, recursive = TRUE)
+  if (length(rscripts) == 0) {
+    print("[ERROR] Found no related R script libraries to run hlalohReforged")
+    quit(status = 1)
+  }
+  invisible(sapply(rscripts, source))
+}
+
+main <- function() {
+  initialize_libs()
+
+  gamma <- 1
+
+  args <- parse_cmd()
+
+  parse_file_path(file = args$tbam)
+  parse_file_path(file = args$nbam)
+  parse_dir_path(dir = args$outdir, create = TRUE)
+
+  tid <- extract_rgsm_from_bam_header(bam = args$tbam)
+  nid <- extract_rgsm_from_bam_header(bam = args$nbam)
+
+  purity <- 0.5
+  ploidy <- 2
+  if (!is.null(args$tstates)) {
+    tstates <- extract_tstates(tstate_est_file = args$tstates)
+    if (!is.na(tstates$ploidy) && !is.na(tstates$purity)) {
+      ploidy <- tstates$ploidy
+      purity <- tstates$purity
+    }
+  }
+  print(paste("[INFO] Purity = ", purity, " Ploidy = ", ploidy, sep = ""))
+
+  alleles_n <- extract_seqinfo_from_bam(bam = args$nbam)
+  alleles_t <- extract_seqinfo_from_bam(bam = args$tbam)
+  # extract function returns named integer vector
+  # here we need names of that vector to get allele name
+  alleles_n <- sort(names(alleles_n))
+  alleles_t <- sort(names(alleles_t))
+  if (!all.equal(alleles_n, alleles_t)) {
+    print("[ERROR] Different set of alleles detected in normal and tumor BAMs")
+    print("[ERROR] Alleles in normal BAM: ", paste(alleles_n, collapse = "|"))
+    print("[ERROR] Alleles in tumor BAM: ", paste(alleles_t, collapse = "|"))
+    quit(status = 1)
+  }
+
+  alleles_dt <- data.table(Alleles = alleles_n)
+  alleles_dt[, HLAGene := tstrsplit(Alleles, "_", keep = 2)]
+  alleles_dt[, HLAGene := paste("hla_", HLAGene, sep = "")]
+  alleles_dt[, Alleles := paste(Alleles, collapse = ","), by = HLAGene]
+  alleles_dt[, c("A1", "A2") := tstrsplit(Alleles, ","), by = HLAGene]
+  alleles_dt[, Alleles := NULL]
+  alleles_dt <- unique(alleles_dt, by = "HLAGene")
+
+  # filter input bams by ecnt
+  filt_nbam <- file.path(args$outdir, paste(nid, ".filt.bam", sep = ""))
+  filt_tbam <- file.path(args$outdir, paste(tid, ".filt.bam", sep = ""))
+  if (!file.exists(filt_nbam) || !file.exists(filt_tbam)) {
+    filter_bam_by_ecnt(
+      bam = args$nbam, obam = filt_nbam, min_necnt = args$min_necnt
+    )
+    filter_bam_by_ecnt(
+      bam = args$tbam, obam = filt_tbam, min_necnt = args$min_necnt
+    )
+  }
+
+  n_seq_depth <- estimate_dp(filt_nbam, alleles = alleles_n)
+  t_seq_depth <- estimate_dp(filt_tbam, alleles = alleles_n)
+  corrector <- n_seq_depth / t_seq_depth
+  print(paste("[INFO] Global depth corrector: ", corrector, sep=""))
+
+  loh_res_dt <- alleles_dt[, call_hla_loh(
+    .SD,
+    tbam = filt_tbam, nbam = filt_nbam, hlaref = args$hlaref,
+    outdir = args$outdir, purity = purity, ploidy = ploidy,
+    min_dp = args$min_cov, min_necnt = args$min_nm,
+    corrector = corrector, gamma = gamma
+  ), by = "HLAGene"]
+  out_res <- file.path(args$outdir, paste(args$subject, ".loh.res.tsv", sep = ""))
+  fwrite(loh_res_dt, out_res, sep = "\t", row.names = FALSE, quote = FALSE)
+}
+
+main()
---
+++ R/lohhlaplot.R
@@ -0,0 +1,252 @@
+#!/usr/bin/env -S Rscript --vanilla
+
+suppressPackageStartupMessages({
+  library(data.table)
+  library(ggplot2)
+  library(argparse)
+})
+
+options(width = 600)
+
+grp4_colors <- c("#0072B5FF", "#E18727FF", "#925E9FFF", "#AD002AFF")
+a1_color <- grp4_colors[1]
+a2_color <- grp4_colors[2]
+
+parse_cmd <- function() {
+  parser <- ArgumentParser()
+  parser$add_argument("--sample",
+    metavar = "STR", type = "character", required = TRUE,
+    help = "Specify sample ID/name"
+  )
+  parser$add_argument("--loh_res",
+    metavar = "FILE", type = "character", required = TRUE,
+    help = "Specify path to the HLA LOH result"
+  )
+  parser$add_argument("--loh_dir",
+    metavar = "DIR", type = "character", required = TRUE,
+    help = "Specify path to the base directory of HLA LOH result"
+  )
+  parser$parse_args()
+}
+
+init_plot_params <- function() {
+  plot_theme <- theme(
+    axis.text.x = element_text(size = 14, color = "black"),
+    axis.text.y = element_text(size = 14, color = "black"),
+    axis.title.x = element_text(size = 14, color = "black"),
+    axis.title.y = element_text(size = 14, color = "black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"),
+    strip.background = element_blank(),
+    strip.text.x = element_blank(),
+    legend.position = "bottom",
+    legend.justification = c("center"),
+    legend.key = element_blank(),
+    legend.text = element_text(size = 12)
+  )
+  list(theme = plot_theme)
+}
+
+plot_cov_profile <- function(
+    cov_dt, a1, a2, cov_col, out,
+    mask = FALSE, height = 6, width = 12) {
+  if (mask) {
+    cov_dt[seqnames == a1, seqnames := "Allele1"]
+    cov_dt[seqnames == a2, seqnames := "Allele2"]
+    a1 <- "Allele1"
+    a2 <- "Allele2"
+  }
+  plot_params <- init_plot_params()
+  max_x <- max(cov_dt$pos)
+  max_y <- max(cov_dt[[cov_col]])
+  m <- ggplot(cov_dt, aes(x = pos, y = cov_dt[[cov_col]])) +
+    geom_line(aes(color = seqnames), linewidth = 1.2) +
+    scale_color_manual(
+      name = "",
+      values = c(a1_color, a2_color),
+      limits = c(a1, a2)
+    ) +
+    scale_x_continuous(expand = c(0, 0), limits = c(0, max_x + 200)) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, max_y + 30)) +
+    labs(x = "Position", y = "Depth") +
+    plot_params$theme
+  ggsave(out, m, width = width, height = height)
+}
+
+plot_tn_cov_profile <- function(
+    cov_dt, corrector, a1, a2, out,
+    mask = FALSE, height = 6, width = 12) {
+  if (mask) {
+    cov_dt[seqnames == a1, seqnames := "Allele1"]
+    cov_dt[seqnames == a2, seqnames := "Allele2"]
+    a1 <- "Allele1"
+    a2 <- "Allele2"
+  }
+  plot_params <- init_plot_params()
+  max_x <- max(cov_dt$pos)
+  min_y <- min(
+    log10(cov_dt$t_dp * corrector), log10(cov_dt$n_dp)
+  )
+  max_y <- max(
+    log10(cov_dt$t_dp * corrector), log10(cov_dt$n_dp)
+  )
+  m <- ggplot(
+    cov_dt,
+    aes(x = pos, y = log10(t_dp * corrector), color = seqnames)
+  ) +
+    geom_line(linewidth = 0.8) +
+    geom_line(
+      data = cov_dt,
+      aes(x = pos, y = log10(n_dp)), linetype = "dotdash"
+    ) +
+    facet_wrap(~seqnames, nrow = 2, scales = "free") +
+    scale_color_manual(
+      name = "",
+      values = c(a1_color, a2_color),
+      limits = c(a1, a2)
+    ) +
+    scale_x_continuous(expand = c(0, 0), limits = c(0, max_x + 200)) +
+    scale_y_continuous(expand = c(0, 0), limits = c(min_y - 1, max_y + 1)) +
+    labs(x = "Position", y = "Depth") +
+    plot_params$theme
+  ggsave(out, m, width = width, height = height)
+}
+
+plot_logr_profile <- function(
+    cov_dt, a1, a2, out,
+    mask = FALSE, height = 6, width = 12) {
+  if (mask) {
+    cov_dt[seqnames == a1, seqnames := "Allele1"]
+    cov_dt[seqnames == a2, seqnames := "Allele2"]
+    a1 <- "Allele1"
+    a2 <- "Allele2"
+  }
+  a1_median_logr <- median(cov_dt[seqnames == a1]$logR, na.rm = TRUE)
+  a2_median_logr <- median(cov_dt[seqnames == a2]$logR, na.rm = TRUE)
+  plot_params <- init_plot_params()
+  max_x <- max(cov_dt$pos)
+  min_y <- min(cov_dt$logR)
+  max_y <- max(cov_dt$logR)
+  m <- ggplot(cov_dt, aes(x = pos, y = logR)) +
+    geom_line(aes(color = seqnames), linewidth = 1.2) +
+    geom_hline(
+      aes(yintercept = a1_median_logr),
+      color = a1_color, linetype = "dashed"
+    ) +
+    geom_hline(
+      aes(yintercept = a2_median_logr),
+      color = a2_color, linetype = "dashed"
+    ) +
+    scale_color_manual(
+      name = "",
+      values = c(a1_color, a2_color),
+      limits = c(a1, a2)
+    ) +
+    scale_x_continuous(expand = c(0, 0), limits = c(0, max_x + 200)) +
+    scale_y_continuous(expand = c(0, 0), limits = c(min_y - 1, max_y + 1)) +
+    labs(x = "Position", y = "LogR") +
+    plot_params$theme
+  ggsave(out, m, width = width, height = height)
+}
+
+plot_baf_profile <- function(mm_dt, out, mask = FALSE, height = 6, width = 12) {
+  if (mask) {
+    mm_dt[, a1_seqnames := "Allele1"]
+  }
+  a1 <- unique(mm_dt$a1_seqnames)
+  plot_params <- init_plot_params()
+  max_x <- max(mm_dt$a1_pos)
+  m <- ggplot(data = mm_dt, aes(x = a1_pos, y = baf_correct)) +
+    geom_point(aes(color = a1_seqnames), size = 1.5, stroke = NA) +
+    geom_hline(aes(yintercept = 0.5), color = "grey", linetype = "dashed") +
+    geom_hline(aes(yintercept = 1.0), color = "grey", linetype = "dashed") +
+    scale_color_manual(
+      name = "",
+      values = c(a1_color),
+      limits = c(a1)
+    ) +
+    scale_x_continuous(expand = c(0, 0), limits = c(0, max_x + 50)) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, 1.2)) +
+    labs(x = "Position", y = "B-allele Frequency") +
+    plot_params$theme
+  ggsave(out, m, width = width, height = height)
+}
+
+plot_profile <- function(hla_gene, dat, plot_dir) {
+  mm <- dat$mm
+  cov_dt <- dat$cov_dt
+  corrector <- dat$corrector
+  alleles <- unique(cov_dt$seqnames)
+  a1 <- alleles[1]
+  a2 <- alleles[2]
+  a1_aln_start <- mm$a1$start
+  a2_aln_start <- mm$a2$start
+  cov_dt[seqnames == a1, pos := pos + a2_aln_start - 1]
+  cov_dt[seqnames == a2, pos := pos + a1_aln_start - 1]
+  out <- file.path(plot_dir, paste(hla_gene, ".t_dp.pdf", sep = ""))
+  plot_cov_profile(
+    cov_dt = cov_dt, a1 = a1, a2 = a2, cov_col = "t_dp", out = out
+  )
+  out <- file.path(plot_dir, paste(hla_gene, ".n_dp.pdf", sep = ""))
+  plot_cov_profile(
+    cov_dt = cov_dt, a1 = a1, a2 = a2, cov_col = "n_dp", out = out
+  )
+  out <- file.path(plot_dir, paste(hla_gene, ".tn_dp.pdf", sep = ""))
+  plot_tn_cov_profile(
+    cov_dt = cov_dt, corrector = corrector, a1 = a1, a2 = a2, out = out
+  )
+  out <- file.path(plot_dir, paste(hla_gene, ".logR.pdf", sep = ""))
+  plot_logr_profile(
+    cov_dt = cov_dt, a1 = a1, a2 = a2, out = out
+  )
+  out <- file.path(plot_dir, paste(hla_gene, ".baf.pdf", sep = ""))
+  plot_baf_profile(mm_dt = dat$mm_dt, out = out)
+}
+
+plot_hlaloh <- function(dt, sample, wkdir) {
+  hla_gene <- dt[["HLAGene"]]
+
+  hla_gene_dat_file <- file.path(wkdir, paste(hla_gene, ".rds", sep = ""))
+  if (!file.exists(hla_gene_dat_file)) {
+    print(paste(
+      "[INFO] Found no intermediate data for HLA gene: ",
+      hla_gene_dat_file,
+      sep = ""
+    ))
+    print("[INFO] Move next HLA gene to plot")
+    return(NULL)
+  }
+  hla_gene_dat <- readRDS(hla_gene_dat_file)
+  contents <- ls(hla_gene_dat)
+  if (length(contents) == 0) {
+    print(paste(
+      "[INFO] Found no content in HLA gene data: ",
+      hla_gene_dat_file,
+      sep = ""
+    ))
+    print("[INFO] Move next HLA gene to plot")
+  }
+
+  plot_dir <- file.path(wkdir, paste(sample, "_plots", sep = ""))
+  if (!dir.exists(plot_dir)) {
+    dir.create(plot_dir, recursive = TRUE)
+  }
+  plot_profile(hla_gene, hla_gene_dat, plot_dir)
+  return(NULL)
+}
+
+run_plot <- function() {
+  args <- parse_cmd()
+
+  loh_res_dt <- fread(args$loh_res)
+
+  loh_res_dt <- loh_res_dt[, apply(
+    .SD, 1, plot_hlaloh,
+    sample = args$sample,
+    wkdir = args$loh_dir
+  )]
+}
+
+run_plot()
