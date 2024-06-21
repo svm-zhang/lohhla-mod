9cadb4a0f580b48217292ee3db5644246b6c268b
simo <svm.zhang@gmail.com>
2024-06-21 10:18:40 -0700

    Update to lohhlamod

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib/R/library/hlalohReforged"
+mkdir -p "$PREFIX/lib/R/library/lohhlamod"
 
 # copy scripts to bin folder
 cp "$RECIPE_DIR/R/lohhlamod.R" "$PREFIX/bin/lohhlamod"
@@ -13,8 +13,8 @@ cp "$RECIPE_DIR/R/lohhlaplot.R" "$PREFIX/bin/lohhlaplot"
 chmod +x "$PREFIX"/bin/*
 
 # copy perl module to lib folder
-cp "$RECIPE_DIR/R/bamer.R" "$PREFIX/lib/R/library/hlalohReforged"
-cp "$RECIPE_DIR/R/cli.R" "$PREFIX/lib/R/library/hlalohReforged"
-cp "$RECIPE_DIR/R/loh.R" "$PREFIX/lib/R/library/hlalohReforged"
-cp "$RECIPE_DIR/R/pairwise_aln.R" "$PREFIX/lib/R/library/hlalohReforged"
-cp "$RECIPE_DIR/R/pathio.R" "$PREFIX/lib/R/library/hlalohReforged"
+cp "$RECIPE_DIR/R/bamer.R" "$PREFIX/lib/R/library/lohhlamod"
+cp "$RECIPE_DIR/R/cli.R" "$PREFIX/lib/R/library/lohhlamod"
+cp "$RECIPE_DIR/R/loh.R" "$PREFIX/lib/R/library/lohhlamod"
+cp "$RECIPE_DIR/R/pairwise_aln.R" "$PREFIX/lib/R/library/lohhlamod"
+cp "$RECIPE_DIR/R/pathio.R" "$PREFIX/lib/R/library/lohhlamod"
